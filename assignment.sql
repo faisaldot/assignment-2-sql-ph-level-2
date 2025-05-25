38f9f0d0d9db5b0c8ac76c826f1630f1f81b8cc1
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-05-25 15:18:01 +0600

    complete task six

--- assignment.sql
+++ assignment.sql
@@ -67,3 +67,10 @@ SELECT s.common_name
 FROM species AS s
 LEFT JOIN sightings AS si ON s.species_id = si.species_id
 WHERE si.species_id IS NULL;
+
+-- Task six: Show the most recent 2 sightings.
+SELECT sp.common_name, si.sighting_time, r.name
+FROM sightings AS si
+JOIN species AS sp ON si.species_id = sp.species_id
+JOIN rangers AS r ON si.ranger_id = r.ranger_id
+ORDER BY si.sighting_time DESC LIMIT 2;
